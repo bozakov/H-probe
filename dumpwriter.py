ee4eba33a5b4d71104a04a5508be92c246716e35
Zdravko Bozakov <zb@ikt.uni-hannover.de>
2012-08-14 11:16:29 +0200

    fixed wrong slots

--- dumpwriter.py
+++ dumpwriter.py
@@ -13,59 +13,50 @@ options = hphelper.options
 DEBUG = hphelper.DEBUG
 
 
-def dumpwriter(pipe, ns, slottimes):
-
-    timetime = time.time
+def dumpwriter(pipe, ns, ST=None):
     hphelper.set_affinity('parser') 
-
-
-    slots = slottimes[:]
-
     DEBUG('starting ' + __name__)
-    run_start = timetime()
-
 
     stats = hphelper.stats_stats()
-
 
     # start progress bar thread
     hphelper.bar_init(options, stats)
 
-
+    if not options.start_time:
+        options.start_time = time.time()
+  
     if not options.savefile:
         # default save name is destination + YYMMDD + HHMM
-        options.savefile = options.DST + time.strftime("_%Y%m%d_%H%M", time.localtime()) + options.tag 
-    options.savefile += '.dump'
-    print "saving RTTs to " + options.savefile + " ..."
+        options.savefile = options.DST + time.strftime("_%Y%m%d_%H%M", 
+                                                       time.localtime(options.start_time)) 
 
+    options.savefile += options.tag 
+    options.savefile += '.dump'
+
+    print "saving RTTs to " + options.savefile + " ..."
 
     try:
             fs = open(options.savefile, mode='w')
-            fs.write('% ' + options.IPDST + ' ' + str(options) + '\n')                # save options
+            fs.write('% ' + 'options:' + ' ' + str(options) + '\n')    # save options
     except KeyboardInterrupt:
             print 'canceled writing file.'
 
-
-    #block until sender + receiver say they are ready                                                    
+    #block until sender + receiver say they are ready
     while not all([ns.RCV_READY,ns.SND_READY]):
         time.sleep(0.1)
 
 
     stats.run_start = time.time()
-    options.start_time = time.time()
-
     try:
        while 1:
-            (seq, snd_time, rtt) = pipe.recv()
-            currslot = slots[stats.seq]
+            (seq, slot, rtt) = pipe.recv()
+            stats.update(seq, rtt=rtt, current_slot=slot)
 
-            stats.update(seq, rtt=rtt, current_slot=currslot)
-
-            if snd_time == -1:
+            if slot == -1:
                  rtt = -1
                  stats.snd_err += 1
 
-            fs.write("%d %d %.9f\n" % (seq, currslot , rtt))
+            fs.write("%d %d %.9f\n" % (seq, slot , rtt))
     except (ValueError, KeyboardInterrupt) as e:
         print 
         DEBUG( __name__ + ': done')
